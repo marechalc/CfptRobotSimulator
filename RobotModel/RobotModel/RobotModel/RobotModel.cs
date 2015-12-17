787d06f89ad7acfcc897e850362b549ffabc8ddb
floxblah <florian.hassler1989@gmail.com>
2015-12-17 11:46:53 +0100

    empty app

---
+++ RobotModel/RobotModel/RobotModel/RobotModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace RobotModel
+{
+    class RobotModel
+    {
+
+        Point _position;
+        string _name;
+        int _position;
+        int _orientation;
+        int _speed;
+        
+        public RobotModel()
+        {
+
+        }
+
+
+
+    }
+}
