464293efa26efafce2a29e24d4d4b23f2079beee
floxblah <florian.hassler1989@gmail.com>
2016-01-14 11:10:09 +0100

    add the Simulator engine project and add properties to it and add properties to robotSimulator

---
+++ SimulatorEngine/SimulatorEngine/SimulatorEngine/SimulatorEngineModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace SimulatorEngine
+{
+    class SimulatorEngineModel
+    {
+
+        private Controller _controller;
+
+        internal Controller Controller
+        {
+            get { return _controller; }
+            set { _controller = value; }
+        }
+       private List<RobotModel> _robots;
+       private SurfaceModel _surface;
+       private Timer _simulationTime;
+
+       public Timer SimulationTime
+       {
+           get { return _simulationTime; }
+           set { _simulationTime = value; }
+       }
+
+        public SimulatorEngineModel(Controller ctrlr)
+        {
+            this.Controller = ctrlr;
+            SimulationTime = new Timer();
+            SimulationTime.Elapsed += Update;
+
+        }
+
+        void Update(object sender, ElapsedEventArgs e)
+        {
+           
+
+        }
+
+
+
+
+
+    }
+}
