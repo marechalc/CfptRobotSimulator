c1708d135c5f7767fe9a5210b9c7c764c805ec84
floxblah <florian.hassler1989@gmail.com>
2016-01-14 11:12:51 +0100

    add the Simulator engine project and add properties to it and add properties to robotSimulator

--- RobotModel/RobotModel/RobotModel/RobotModel.cs
+++ RobotModel/RobotModel/RobotModel/RobotModel.cs
@@ -1,25 +1,89 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Drawing;
 
 namespace RobotModel
 {
     class RobotModel
     {
+        private const int DEFAULT_SPEED = 0;
 
+        private SimulatorEngineModel _simulatorEngine;
         Point _position;
-        string _name;
-        int _position;
-        int _orientation;
         int _speed;
-        
-        public RobotModel()
+        string _name;
+        int _orientation;
+        string _instruction;
+
+        public SimulatorEngineModel SimulatorEngine
+        {
+            get { return _simulatorEngine; }
+            set { _simulatorEngine = value; }
+        }
+
+        public string Instruction
+        {
+            get { return _instruction; }
+            set { _instruction = value; }
+        }
+
+        public Point Position
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+
+        public int Orientation
+        {
+            get { return _orientation; }
+            set { _orientation = value; }
+        }
+
+
+        public int Speed
+        {
+            get { return _speed; }
+            set { _speed = value; }
+        }
+
+        public RobotModel(SimulatorEngineModel simulatorEngine, string name, Point position_xy, int orientation, string instruction)
+            : this(simulatorEngine, name, position_xy.X, position_xy.Y, orientation, instruction)
+        {
+            //no code here
+        }
+
+        public RobotModel(SimulatorEngineModel simulatorEngine, string name, int position_x, int position_y, int orientation, string instruction)
+        {
+            this.SimulatorEngine = simulatorEngine;
+            this.Name = name;
+            this.Position = new Point(position_x, position_y);
+            this.Orientation = orientation;
+            this.Instruction = instruction;
+        }
+
+        public void update()
         {
 
         }
+
+
+
+     /* public string Log(string message)
+        {
+            return DateTime.Now.ToString("h:mm:ss:fff") + message;
+        }
+
+        public string Log(string property, string value)
+        {
+            return DateTime.Now.ToString("h:mm:ss:fff") + " - " + property + " = " + value;
+        }*/
+
 
 
 
