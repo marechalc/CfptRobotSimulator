c651996be68211ded24c4c5e2267ddfbba86aef6
R3 <s.cardoso@threeangle.ch>
2016-01-21 09:54:48 +0100

    Merged class RobotModel, Added StrategyParser to the SimulatorEngine Project
    Merged the class RobotModel with the project SimulatorEngine, deleted
    the project RobotModel and added the StrategyParser method to the
    SimulatorEngine Project

--- SimulatorEngine/SimulatorEngine/SimulatorEngine/RobotModel.cs
+++ SimulatorEngine/SimulatorEngine/SimulatorEngine/RobotModel.cs
@@ -1,12 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 
 namespace SimulatorEngine
 {
     class RobotModel
     {
+        private const int DEFAULT_SPEED = 0;
+
+        private SimulatorEngineModel _simulatorEngine;
+        Point _position;
+        int _speed;
+        string _name;
+        int _orientation;
+        string _instruction;
+
+        public SimulatorEngineModel SimulatorEngine
+        {
+            get { return _simulatorEngine; }
+            set { _simulatorEngine = value; }
+        }
+
+        public string Instruction
+        {
+            get { return _instruction; }
+            set { _instruction = value; }
+        }
+
+        public Point Position
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Orientation
+        {
+            get { return _orientation; }
+            set { _orientation = value; }
+        }
+
+        public int Speed
+        {
+            get { return _speed; }
+            set { _speed = value; }
+        }
+
+        public RobotModel(SimulatorEngineModel simulatorEngine, string name, Point position_xy, int orientation, string instruction)
+            : this(simulatorEngine, name, position_xy.X, position_xy.Y, orientation, instruction)
+        {
+            //no code here
+        }
+
+        public RobotModel(SimulatorEngineModel simulatorEngine, string name, int position_x, int position_y, int orientation, string instruction)
+        {
+            this.SimulatorEngine = simulatorEngine;
+            this.Name = name;
+            this.Position = new Point(position_x, position_y);
+            this.Orientation = orientation;
+            this.Instruction = instruction;
+        }
+
+        public void update()
+        {
+
+        }
     }
 }
--- SimulatorEngine/SimulatorEngine/SimulatorEngine/SimulatorEngineModel.cs
+++ SimulatorEngine/SimulatorEngine/SimulatorEngine/SimulatorEngineModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,10 @@ namespace SimulatorEngine
 {
     class SimulatorEngineModel
     {
-
+        private const String VI_PATTERN = "";
+        private List<RobotModel> _robots;
+        private SurfaceModel _surface;
+        private Timer _simulationTime;
         private Controller _controller;
 
         internal Controller Controller
@@ -17,9 +21,6 @@ namespace SimulatorEngine
             get { return _controller; }
             set { _controller = value; }
         }
-       private List<RobotModel> _robots;
-       private SurfaceModel _surface;
-       private Timer _simulationTime;
 
        public Timer SimulationTime
        {
@@ -27,23 +28,38 @@ namespace SimulatorEngine
            set { _simulationTime = value; }
        }
 
+        /// <summary>
+        /// Constructor by default
+        /// </summary>
+        /// <param name="ctrlr"></param>
         public SimulatorEngineModel(Controller ctrlr)
         {
             this.Controller = ctrlr;
             SimulationTime = new Timer();
             SimulationTime.Elapsed += Update;
-
         }
 
+        /// <summary>
+        /// Tick of the timer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         void Update(object sender, ElapsedEventArgs e)
         {
            
 
         }
 
+        public void StrategyParser()
+        {
+            string line = "";
+            StreamReader file = new StreamReader(SimulatorEngine.Properties.Resources.test_parcours_carre);
+            RobotModel robot = new RobotModel(this, "RB1", 0, 0, 0, "");
 
-
-
-
+            while((line = file.ReadLine()) != null)
+            {
+                robot.Instruction = line;
+            }
+        }
     }
 }
