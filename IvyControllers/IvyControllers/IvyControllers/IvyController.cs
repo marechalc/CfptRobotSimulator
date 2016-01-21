db3bf3f7a99d426d159b311cdd59e29ee7ee6aa3
Dimitri Lizzi <araxor@gmail.com>
2016-01-21 10:52:05 +0100

    Simple version of IvyController that can send robots position and orientation

--- IvyControllers/IvyControllers/IvyControllers/IvyController.cs
+++ IvyControllers/IvyControllers/IvyControllers/IvyController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using IvyBus;
+using System.Text.RegularExpressions;
 
 
 namespace IvyControllers
@@ -32,15 +33,28 @@ namespace IvyControllers
         /// </summary>
         public const string DEFAULT_BUS_READY_MESSAGE = "READY";
         
+
+
+        public const string POSITION_CHANGED_REGEX = @"^PositionChanged ([A-Za-z]+) ([0-9]+) ([0-9]+)";
+        public const string ORIENTATION_CHANGED_REGEX = @"^RotationChanged ([A-Za-z]+) ([0-9]+\.?\[0-9]*)";
         #endregion
 
-        #region Properties
+        #region Private properties
         
         /// <summary>
         /// Provides connection to a shared network bus, used to broadcast information through network.
         /// </summary>
-        protected Ivy IvyBus { get; set; }
+        private Ivy IvyBus { get; set; }
 
+
+        #endregion
+
+        #region Events
+        public delegate void PositionChangedEventHandler(string robotName, int x, int y);
+        public delegate void OrientationChangedEventHandler(string robotName, double orientation);
+        
+        public event PositionChangedEventHandler PositionChanged;
+        public event OrientationChangedEventHandler OrientationChanged;
         #endregion
 
         #region Constructors
@@ -55,8 +69,10 @@ namespace IvyControllers
         public IvyController(string name, string readyMessage, string messageFilter, string broadcastAddress, string broadcastPort)
         {
             IvyBus = new Ivy(name, readyMessage);
-            IvyBus.BindMsg(messageFilter, MessageReceivedCallback);
             IvyBus.Start(String.Format("{0}:{1}", broadcastAddress, broadcastPort));
+
+            IvyBus.BindMsg(POSITION_CHANGED_REGEX, OnPositionChangedReceived);
+            IvyBus.BindMsg(ORIENTATION_CHANGED_REGEX, OnRotationChangedReceived);
         }
 
         /// <summary>
@@ -78,25 +94,64 @@ namespace IvyControllers
         { }
         #endregion
 
-        #region Methods
-        /// <summary>
-        /// Sends the given message through the bus.
-        /// </summary>
-        /// <param name="args"></param>
-        public void SendMessageThroughBus(object[] args)
-        {
-            IvyBus.SendMsg("", args);
-        }
+        #region Private Methods
 
         /// <summary>
-        /// Called on every message received through the bus.
+        /// Called by IvyBus when a rotation change message has been received
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        internal virtual void MessageReceivedCallback(object sender, IvyMessageEventArgs e)
+        private void OnRotationChangedReceived(object sender, IvyMessageEventArgs e)
         {
+            Regex regex = new Regex(POSITION_CHANGED_REGEX);
 
+            Match result = regex.Match(e.Argument);
+
+            if (result.Captures.Count == 3)
+            {
+                string robotName = result.Captures[0].Value;
+                int x = Convert.ToInt32(result.Captures[1].Value);
+                int y = Convert.ToInt32(result.Captures[2].Value);
+
+                PositionChanged(robotName, x, y);
+            }
+        }
+
+        /// <summary>
+        /// Called by IvyBus when an orientation change message has been received
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnPositionChangedReceived(object sender, IvyMessageEventArgs e)
+        {
+            Regex regex = new Regex(ORIENTATION_CHANGED_REGEX);
+
+            Match result = regex.Match(e.Argument);
+
+            if (result.Captures.Count == 2)
+            {
+                string robotName = result.Captures[0].Value;
+                double angle = Convert.ToDouble(result.Captures[1].Value);
+
+                OrientationChanged(robotName, angle);
+            }
+        }
+
+        #endregion
+
+        #region Public methods
+        public void SendPositionChange(string robotName, int x, int y)
+        {
+            IvyBus.SendMsg(String.Format("PositionChanged {0} {1} {2}",
+                robotName, x, y));
+        }
+
+        public void SendOrientationChanged(string robotName, double angle)
+        {
+            IvyBus.SendMsg(String.Format("OrientationChanged {0} {1}",
+                robotName, angle));
         }
         #endregion
+
     }
 }
