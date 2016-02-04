e2fb48af58318e741d7d9c6a5a559afc6a5003f4
R3 <s.cardoso@threeangle.ch>
2016-02-04 11:42:21 +0100

    View now use the Robot class

--- CRSView/CRSView/View.Designer.cs
+++ CRSView/CRSView/View.Designer.cs
@@ -28,21 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.ssView = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.ssView.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // ssView
+            // 
+            this.ssView.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.ssView.Location = new System.Drawing.Point(0, 588);
+            this.ssView.Name = "ssView";
+            this.ssView.Size = new System.Drawing.Size(905, 22);
+            this.ssView.TabIndex = 0;
+            this.ssView.Text = "statusStrip";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(70, 17);
+            this.toolStripStatusLabel1.Text = "tlsInstruction";
             // 
             // View
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(905, 610);
+            this.Controls.Add(this.ssView);
             this.Name = "View";
             this.Text = "CFPT Robot Simulator";
             this.Load += new System.EventHandler(this.View_Load);
+            this.ssView.ResumeLayout(false);
+            this.ssView.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.StatusStrip ssView;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
     }
 }
 
