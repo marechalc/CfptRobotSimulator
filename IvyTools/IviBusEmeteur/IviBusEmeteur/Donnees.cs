a76e1beeca33ee51e9621a11f0f15893eb186358
GillesBergerat <gilles.bergerat@hotmail.com>
2016-03-10 10:38:42 +0100

    Change Ivy.dll path

---
+++ IvyTools/IviBusEmeteur/IviBusEmeteur/Donnees.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IviBusEmeteur
+{
+    public class Donnees
+    {
+
+      
+        public string envoiDonneesX()
+        {
+              Random r = new Random();
+            /*int x = 1;
+            for (int i = 0; i < 10; i++)
+            {
+                x += 5;
+            }*/
+            int posX = r.Next(0, 360);
+         //  return " ---Time : 12951360388501 INFO : SPIDER : Posx reelle : "+ posX  +" Position calcul " + (posX+20);
+            return "---Time : 12951360388625 INFO : ENVOI BLUETOOTH : ST";
+        }
+
+        public string envoiDonneesY()
+        {
+            int y = 1;
+            for (int i = 0; i < 10; i++)
+            {
+                y += 10;
+            }
+
+            //return "Y = " + y.ToString();
+            return "---Time : 12951360390425 INFO : RECU BLUETOOTH : AR0,0740,0058";
+        }
+
+        public string orientationChanged()
+        {
+            Random r = new Random();
+
+            int posX = r.Next(0, 360);
+            int robot = r.Next(1, 4);
+
+            return "OrientationChanged " + " Robot " + robot + ' ' + " orientation : " + posX + " degrees" ;
+        }
+
+        public string positionChanger()
+        {
+            
+            Random r = new Random();
+
+            int posX = r.Next(0,500);
+            int posY = r.Next(0,250);
+            int robot = r.Next(1, 4);
+          
+            return "PositionChanged " + " Robot " + robot + ' ' + " posX : " + posX + ' '  + " posY : " + posY;
+        }
+    }
+}
---
+++ IvyTools/IviBusEmeteur/IviBusEmeteur/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace IviBusEmeteur
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnConnexion = new System.Windows.Forms.Button();
+            this.tbxAdresse = new System.Windows.Forms.TextBox();
+            this.tbxMessage = new System.Windows.Forms.TextBox();
+            this.btnEnvoyer = new System.Windows.Forms.Button();
+            this.btnDeconnexion = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnConnexion
+            // 
+            this.btnConnexion.Location = new System.Drawing.Point(63, 38);
+            this.btnConnexion.Name = "btnConnexion";
+            this.btnConnexion.Size = new System.Drawing.Size(75, 23);
+            this.btnConnexion.TabIndex = 0;
+            this.btnConnexion.Text = "Connexion";
+            this.btnConnexion.UseVisualStyleBackColor = true;
+            this.btnConnexion.Click += new System.EventHandler(this.btnConnexion_Click);
+            // 
+            // tbxAdresse
+            // 
+            this.tbxAdresse.Location = new System.Drawing.Point(12, 12);
+            this.tbxAdresse.Name = "tbxAdresse";
+            this.tbxAdresse.Size = new System.Drawing.Size(180, 20);
+            this.tbxAdresse.TabIndex = 1;
+            this.tbxAdresse.Text = "127.0.0.1";
+            // 
+            // tbxMessage
+            // 
+            this.tbxMessage.Location = new System.Drawing.Point(12, 112);
+            this.tbxMessage.Name = "tbxMessage";
+            this.tbxMessage.Size = new System.Drawing.Size(180, 20);
+            this.tbxMessage.TabIndex = 2;
+            // 
+            // btnEnvoyer
+            // 
+            this.btnEnvoyer.Location = new System.Drawing.Point(38, 138);
+            this.btnEnvoyer.Name = "btnEnvoyer";
+            this.btnEnvoyer.Size = new System.Drawing.Size(116, 23);
+            this.btnEnvoyer.TabIndex = 3;
+            this.btnEnvoyer.Text = "Envoyer le message";
+            this.btnEnvoyer.UseVisualStyleBackColor = true;
+            this.btnEnvoyer.Click += new System.EventHandler(this.btnEnvoyer_Click);
+            // 
+            // btnDeconnexion
+            // 
+            this.btnDeconnexion.Location = new System.Drawing.Point(12, 211);
+            this.btnDeconnexion.Name = "btnDeconnexion";
+            this.btnDeconnexion.Size = new System.Drawing.Size(180, 23);
+            this.btnDeconnexion.TabIndex = 4;
+            this.btnDeconnexion.Text = "Déconnexion";
+            this.btnDeconnexion.UseVisualStyleBackColor = true;
+            this.btnDeconnexion.Click += new System.EventHandler(this.btnDeconnexion_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(207, 262);
+            this.Controls.Add(this.btnDeconnexion);
+            this.Controls.Add(this.btnEnvoyer);
+            this.Controls.Add(this.tbxMessage);
+            this.Controls.Add(this.tbxAdresse);
+            this.Controls.Add(this.btnConnexion);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnexion;
+        private System.Windows.Forms.TextBox tbxAdresse;
+        private System.Windows.Forms.TextBox tbxMessage;
+        private System.Windows.Forms.Button btnEnvoyer;
+        private System.Windows.Forms.Button btnDeconnexion;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
